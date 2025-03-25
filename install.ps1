2293d978ba0e52bb363fced5720407bcccc60255
David McWee <dmcwee@microsoft.com>
2025-03-25 16:19:27 -0400

    fixes for install

--- install.ps1
+++ install.ps1
@@ -13,9 +13,11 @@ if(!(Test-Path -Path .\$moduleName.psm1) -or $force) {
 
     $cert = Get-ChildItem -Path Cert:\CurrentUser\My -DnsName $moduleName@davidmcwee.com -ErrorAction SilentlyContinue
     if(($null -eq $tCert) -or ($force)) {
-        $cert = New-SelfSignedCertificate -DnsName $moduleName@davidmcwee.com -Type CodeSigning -CertStoreLocation Cert:\CurrentUser\My
-        Export-Certificate -Cert $cert -FilePath $moduleName.crt
-        Import-Certificate -FilePath .\$moduleName.crt -CertStoreLocation Cert:\CurrentUser\Root
+        $dnsName = "$moduleName@davidmcwee.com"
+        $crtName = "$moduleName.crt"
+        $cert = New-SelfSignedCertificate -DnsName $dnsName -Type CodeSigning -CertStoreLocation Cert:\CurrentUser\My
+        Export-Certificate -Cert $cert -FilePath $crtName
+        Import-Certificate -FilePath $crtName -CertStoreLocation Cert:\CurrentUser\Root
     }
 
     Set-AuthenticodeSignature .\$moduleName.psm1 -Certificate $cert
